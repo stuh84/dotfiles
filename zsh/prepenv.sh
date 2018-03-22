603ba5ed73d76e76464ffa8e2fefc2f15719491d
Stuart Howlette <stuart.howlette@tfmnetworks.com>
2018-03-22 08:56:57 +0000

    ZSH ENV

--- zsh/prepenv.sh
+++ zsh/prepenv.sh
@@ -4,3 +4,4 @@ mkdir ~/git/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/dylanaraps/neofetch.git ~/git/neofetch && cd ~/git/neofetch && sudo make install
+git clone https://github.com/ryanoasis/nerd-fonts.git ~/git/nerd-fonts && cd ~/git/nerd-fonts && sudo ./install.sh
